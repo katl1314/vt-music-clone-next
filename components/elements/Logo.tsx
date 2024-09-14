c6c6c17e09d54044f68d9e7f49fad1e12780087d
최민혁 <nogoso3@gmail.com>
2024-09-14 11:14:24 +0900

    Fixed: useRouter 컴포넌트 최상위로 이동

--- components/elements/Logo.tsx
+++ components/elements/Logo.tsx
@@ -8,10 +8,11 @@ import IconButton from "./IconButton";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 const Logo = () => {
+  // useRouter를 이용 (appRouter시 next/navigation의 useRouter를 사용한다.)
+  const { push } = useRouter();
+
   // Next.js에서 이벤트 처리, 리액트 훅 처리는 서버 컴포넌트가 아닌 클라이언트 컴포넌트여야한다.
   const onClickLogo = () => {
-    // home으로 이동 => useRouter를 이용 (appRouter시 next/navigation의 useRouter를 사용한다.)
-    const { push } = useRouter();
     push("/"); // Home으로 이동
   };
 
