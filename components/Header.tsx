545c339e11f7fb0d69a9f2bd1de2a0a85fc8ad58
최민혁 <nogoso3@gmail.com>
2024-09-14 22:39:52 +0900

    Update: Tailwindcss를 적용한 헤더 구성

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import UserIcon from "./UserIcon";
+import PageSession from "./PagePadding";
+import { FaChromecast } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
 // next.js에서 외부 이미지를 가져와야할 때 next.config.js에서 수정해야함.
 const Header = ({ children }: React.PropsWithChildren) => {
   return (
@@ -15,6 +19,26 @@ const Header = ({ children }: React.PropsWithChildren) => {
         </div>
         <div className="absolute h-[400px] top-0 bg-black opacity-40 w-full"></div>
         <div className="absolute h-[400px] top-0 bg-gradient-to-t from-black w-full"></div>
+      </section>
+      <section className="sticky">
+        <PageSession>
+          <div className="h-[64px] flex flex-row justify-between items-center">
+            <article className="flex flex-row items-center h-[42px] min-w-[480px] bg-[rgba(0,0,0,0.14)] rounded-2xl px-4 gap-4">
+              <div>
+                <FiSearch size={24} />
+              </div>
+              <input
+                type="text"
+                className="h-full w-full bg-transparent px-[10px]"
+                placeholder="노래, 앨범, 아티스트, 팟캐스트 검색"
+              />
+            </article>
+            <article className="flex flex-row items-center gap-6">
+              <FaChromecast size={26} />
+              <UserIcon />
+            </article>
+          </div>
+        </PageSession>
       </section>
       <section className="absolute">{children}</section>
     </header>
---
+++ components/PagePadding.tsx
@@ -0,0 +1,13 @@
+// 반응형 디자인에 따라 패딩이 달라지기 때문임.
+// tailwindCSS중 클래스명 앞에 추가하는 키워드에 따라 반응형 디자인 적용 가능
+// lg: min-width: 1024px
+// md: min-width: 768px
+// sm: min-width: 640px
+// 위 조건에 따라 스타일을 적용하고 싶을때 사용한다.
+const PagePadding = ({ children }: React.PropsWithChildren) => {
+  return (
+    <div className="mx-auto px-[10px] py-[2px] lg:px-[100px]">{children}</div>
+  );
+};
+
+export default PagePadding;
