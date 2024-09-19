8dd185bb2fda5341e931e1e41981bdc2bc26abc9
최민혁 <nogoso3@gmail.com>
2024-09-20 00:27:31 +0900

    Update: 버튼 추가

--- components/elements/DarkButton.tsx
+++ components/elements/DarkButton.tsx
@@ -1,8 +1,30 @@
+"use client";
+import { cn } from "@/lib/utils";
 import { IButton } from "@/types";
 import React, { FC } from "react";
 
-const DarkButton: FC<IButton> = () => {
-  return <div></div>;
+interface IElement extends IButton {
+  className?: string;
+}
+const DarkButton: FC<IElement> = ({
+  label,
+  icon,
+  onClickButton,
+  className,
+  ...props
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-row gap-2 rounded-2xl border-1 bg-neutral-800 text-white items-center justify-center min-w-[80px] h-[36px] p-2 cursor-pointer hover:bg-neutral-700 border-neutral-700",
+        className
+      )}
+      {...props}
+    >
+      <span>{icon}</span>
+      <span>{label}</span>
+    </div>
+  );
 };
 
 export default DarkButton;
--- components/elements/WhiteButton.tsx
+++ components/elements/WhiteButton.tsx
@@ -1,8 +1,30 @@
+"use client";
+import { cn } from "@/lib/utils";
 import { IButton } from "@/types";
 import React, { FC } from "react";
 
-const WhiteButton: FC<IButton> = () => {
-  return <div></div>;
+interface IElement extends IButton {
+  className?: string;
+}
+const WhiteButton: FC<IElement> = ({
+  label,
+  icon,
+  onClickButton,
+  className,
+  ...props
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-row gap-2 rounded-2xl border-1 bg-white text-black items-center justify-center min-w-[80px] h-[36px] p-2 cursor-pointer hover:bg-neutral-200",
+        className
+      )}
+      {...props}
+    >
+      <span>{icon}</span>
+      <span className="text-[14px]">{label}</span>
+    </div>
+  );
 };
 
 export default WhiteButton;
